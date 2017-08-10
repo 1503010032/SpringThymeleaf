af451c1f21abeff66d5c77cb77fa7dbdd119a2e5
sanjeevkumarpeddi <sanjeevkumarpeddi@10.1.53.207>
2017-08-10 16:19:14 -0400

    SpringThymeleaf

---
+++ src/main/java/com/example/demo/SpringBootLeafApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLeafApplication.class, args);
+	}
+}
